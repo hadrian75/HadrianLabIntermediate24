1bbd3a35163beddac47912d94ac0ee72a2a37a7c
Drian <igedehadrian75@gmail.com>
2025-01-16 23:21:01 +0700

    Add changes for maximum 5 chances

--- Tugas1-Hadrian.py
+++ Tugas1-Hadrian.py
@@ -1,9 +1,15 @@
 answer = ""
 def guess(secretWord):
+    counter = 0
     global answer
     i = 0
     while i != len(secretWord):
+        if(counter == 5):
+            print("\nKesempatan Habis")
+            break
         userInput = input(f"Masukan Huruf {i + 1} :")
+        if(userInput):
+            counter +=1
         if(userInput.strip() != ""):
               if(len(userInput) == 1):
                 if(userInput == secretWord[i]):
@@ -23,7 +29,12 @@ def guess(secretWord):
                 print("Tidak boleh lebih dari 1 huruf")
         else:
             print("Jawaban tidak boleh kosong!")
-    print(f"Jawaban anda benar ({answer})")
+    if(len(answer) == len(secretWord)):
+        print(f"Jawaban anda benar ({answer})")
+    elif(len(answer) < len(secretWord) and answer != ""):
+        print(f"Jawaban anda kurang tepat ({answer})")
+    elif(answer == ""):
+        print("Ups... Belum ada yang kejawab nih")
 guess("Duren")
                 
 
