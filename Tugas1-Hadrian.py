11322b54461ed20c2131b56310bf80a04bd13534
Drian <igedehadrian75@gmail.com>
2025-01-16 15:21:40 +0700

    Push To repo

---
+++ Tugas1-Hadrian.py
@@ -0,0 +1,7 @@
+def askAge():
+    userInput = input("Masukan Umur anda Setelah 50 Tahun :")
+    twoDigits = userInput[0]+userInput[1]
+    result = float("0" + twoDigits)
+    
+    return print()
+askAge()
